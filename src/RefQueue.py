c0dba5b822f6013dd5a5194fa4f94e4995302718
Khokhlakov <86792425+Khokhlakov@users.noreply.github.com>
2022-05-05 21:13:56 -0700

    Update RefQueue.py

--- src/RefQueue.py
+++ src/RefQueue.py
@@ -4,7 +4,6 @@ class RefQueue():
   def __init__(self):
     self.head = None
     self.tail = None
-    self.count = 0
     
   def enqueue(self, data):
     tempVar = Node(data)
@@ -13,13 +12,11 @@ class RefQueue():
     else: 
       self.head = tempVar
     self.tail = tempVar
-    self.count += 1
 
   def dequeue(self):
     if not self.isEmpty():
-      tempVar = self.head
+      tempVar = self.head.getValue()
       self.head = self.head.getNext()
-      self.count -= 1
       if self.isEmpty():
         self.tail = None
       return tempVar
